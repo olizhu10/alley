6536b329822085a7176ec644c095853e70240138
Olivia Zhu <olizhu10@gmail.com>
2020-09-19 12:34:26 -0400

    some stuff

---
+++ Alley/Models/Tip.swift
@@ -0,0 +1,9 @@
+//
+//  TIp.swift
+//  Alley
+//
+//  Created by Olivia on 9/19/20.
+//  Copyright © 2020 Avani Aggrwal. All rights reserved.
+//
+
+import Foundation
---
+++ Alley/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Alley
+//
+//  Created by Olivia on 9/19/20.
+//  Copyright © 2020 Avani Aggrwal. All rights reserved.
+//
+
+import Foundation
