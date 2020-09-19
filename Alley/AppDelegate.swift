ce85500e25534433af635b074bf7bdf43c77b2a3
Olivia Zhu <olizhu10@gmail.com>
2020-09-19 14:57:14 -0400

    mereged?

--- Alley/AppDelegate.swift
+++ Alley/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        // Change the window's root view controller to be a UINavigationController.
+        // This way, we can push new view controllers onto our navigation stack
+        // and use multiple ViewControllers across our app.
+        // The navigation controller must start off with a beginning view controller, or the root.
+        // We want the root view controller to display our ViewController.
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
+        window?.makeKeyAndVisible()
         return true
     }
 
--- Alley/ViewController.swift
+++ Alley/ViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+//import GoogleSignIn
 
 class ViewController: UIViewController {
+    
+    var welcomeLabel: UILabel!
+    var titleLabel: UILabel!
+    var subLabel: UILabel!
+    var continueButton: UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor(hue: 266/360, saturation: 13/100, brightness: 97/100, alpha: 1.0)
+        titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "alley"
+        titleLabel.textColor = .black
+        titleLabel.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+        view.addSubview(titleLabel)
+        subLabel = UILabel()
+        subLabel.translatesAutoresizingMaskIntoConstraints = false
+        subLabel.numberOfLines = 2
+        subLabel.text = "channel your efforts into \n creating a better future."
+        subLabel.lineBreakMode = .byWordWrapping
+        subLabel.textColor = .black
+        subLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        view.addSubview(subLabel)
+        continueButton = UIButton()
+        continueButton.translatesAutoresizingMaskIntoConstraints = false
+        continueButton.setTitle("continue", for: .normal)
+        continueButton.setTitleColor(.blue, for: .normal)
+        continueButton.addTarget(self, action: #selector(pushDashViewController), for: .touchUpInside)
+        view.addSubview(continueButton)
+
+        setupConstraints()
     }
-
-
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.bottomAnchor.constraint(equalTo: view.topAnchor, constant: 400),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+        NSLayoutConstraint.activate([
+            subLabel.bottomAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 75),
+            //            subLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -15),
+            //            subLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 30),
+            subLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+        NSLayoutConstraint.activate([
+            continueButton.bottomAnchor.constraint(equalTo: subLabel.bottomAnchor, constant: 75),
+            //            subLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -15),
+            //            subLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 30),
+            continueButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+    }
+    
+    @objc func pushDashViewController() {
+        let dashViewController = DashViewController()
+//        dashViewController.delegate = self
+//        self.navigationController?.pushViewController(dashViewController, animated: true)
+        present(dashViewController, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+    //        if (error != nil) {
+    //            print("\(error.localizedDescription)")
+    //            return
+    //        }
+    //    }
+    
+    
+    
+    
+    
 }
 
